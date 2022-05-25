1cb476577c6607bb7650cbf406173652d60e467b
Minh Hang Nguyen <hang00103@gmail.com>
2022-05-25 13:52:53 -0400

    refactor to use response.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const { version, author } = require('../../package.json');
 
 // authorization middleware
 const { authenticate } = require('../authorization');
+const { createSuccessResponse } = require('../response');
 
 // Create a router that we can use to mount our API
 const router = express.Router();
@@ -22,13 +23,13 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this...
-    githubUrl: 'https://github.com/minhhang107/fragments',
-    version,
-  });
+  res.status(200).json(
+    createSuccessResponse({
+      author,
+      githubUrl: 'https://github.com/minhhang107/fragments',
+      version,
+    })
+  );
 });
 
 module.exports = router;
--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -5,7 +5,6 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('GET /v1/fragments', () => {
-  console.log(request(app));
   // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
 
