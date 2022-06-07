a224aebe606ecb68f4fc326e6c70609af2e7279e
Minh Hang Nguyen <hang00103@gmail.com>
2022-06-07 16:23:03 -0400

    remove comments

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -4,14 +4,11 @@ const app = require('../../src/app');
 const { readFragment } = require('../../src/model/data');
 
 describe('POST /v1/fragments', () => {
-  // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () => request(app).post('/v1/fragments').expect(401));
 
-  // If the wrong username/password pair are used (no such user), it should be forbidden
   test('incorrect credentials are denied', () =>
     request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
-  // Using a valid username/password pair should give a success result with a .fragments array
   test('authenticated users post a supported fragment', async () => {
     const res = await request(app)
       .post('/v1/fragments')
